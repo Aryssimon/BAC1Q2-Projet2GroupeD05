703aadc21654d264ce4213f1b1c377194a885bf5
Simon Arys <simon.arys@student.uclouvain.be>
2020-04-01 10:22:52 +0200

    modification de coronavirus vers inginious

--- projet/data_base.py
+++ projet/data_base.py
@@ -5,7 +5,7 @@ import sqlite3 as sq
 
 # Accès à la base de données
 
-conn = sqlite3.connect('inginious.sqlite')
+conn = sqlite3.connect('{{ url_for('static',filename='inginious.sqlite') }}')
 
 # Le curseur permettra l'envoi des commandes SQL
 cursor = conn.cursor()
@@ -17,3 +17,51 @@ conn.commit()
 
 # Toujours fermer la connexion quand elle n'est plus utile
 conn.close()
+
+
+id=[]
+for row in cursor.execute("SELECT id FROM submissions"):
+    id.append(row)
+
+course=[]
+for row in cursor.execute("SELECT course FROM submissions"):
+    course.append(row)
+
+status=[]
+for row in cursor.execute("SELECT status FROM submissions"):
+    status.append(row)
+
+submitted_on=[]
+for row in cursor.execute("SELECT submitted_on FROM submissions"):
+    submitted_on.append(row)
+
+username=[]
+for row in cursor.execute("SELECT username FROM submissions"):
+    username.append(row)
+
+response_type=[]
+for row in cursor.execute("SELECT response_type FROM submissions"):
+    response_type.append(row)
+
+grade=[]
+for row in cursor.execute("SELECT grade FROM submissions"):
+    grade.append(row)
+
+result = []
+for row in cursor.execute("SELECT result FROM submissions"):
+    result.append(row)
+
+submission=[]
+for row in cursor.execute("SELECT submission FROM user_tasks "):
+    submission.append(row)
+
+succeeded=[]
+for row in cursor.execute("SELECT succeeded FROM user_tasks "):
+    succeeded.append(row)
+
+tried=[]
+for row in cursor.execute("SELECT tried FROM user_tasks "):
+    tried.append(row)
+
+# def get_datas():
+    #return [nom, points, jesaispas]
--- projet/flask_file.py
+++ projet/flask_file.py
@@ -1,8 +1,7 @@
-# Python file to create a html page with graphics on the covid-19, with the data of https://ourworldindata.org/coronavirus-source-data
+# Main Python file to run our Website with graphics based on Inginious.
 # Authors : Arys Simon, Alsteens Louis, El Ouilinti Aymen.
 
 import page_creator
-import translate_file
 
 
 from flask import Flask, render_template, url_for
@@ -22,10 +21,7 @@ def graphique():
     """
     retourne le contenu de la page graphique.html
     """
-    dates_and_cases = translate_file.file_to_graph('total_cases.csv')
-    dates = dates_and_cases[0]
-    cases = dates_and_cases[1]
-    return page_creator.html_page_creator(dates, cases)
+    return "en travaux" #page_creator.html_page_creator(dates, cases)
 
 if __name__ == '__main__':
     app.run()
