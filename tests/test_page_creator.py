e3c524a552f1cb5f2b88547e0fccdc394f01329a
Simon Arys <simon.arys@student.uclouvain.be>
2020-04-01 09:27:28 +0200

    ajout des fichiers de test vides

---
+++ tests/test_page_creator.py
@@ -0,0 +1,15 @@
+#Python file containing the unittests for the <page_creator.py> file.
+# Authors : Arys Simon, Alsteens Louis, El Ouilinti Aymen.
+
+
+import unittest
+
+
+class Test_Page_Creator(unittest.TestCase):
+
+    def test0_jesaispas(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_translate_file.py
@@ -0,0 +1,16 @@
+#Python file containing the unittests for the <translate_file.py> file.
+# Authors : Arys Simon, Alsteens Louis, El Ouilinti Aymen.
+
+
+
+import unittest
+
+
+class Test_Translate_File(unittest.TestCase):
+
+    def test0_jesaispas(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
