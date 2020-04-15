6b8f2dbb5d708d346a503ec5361a7b01dca5dbeb
Simon Arys <simon.arys@student.uclouvain.be>
2020-04-15 14:56:04 +0200

    mise à jour

--- projet/flask_file.py
+++ projet/flask_file.py
@@ -20,51 +20,45 @@ def get_data_from_dict(cours):
         datas[1].append(y)
     return datas
 
+
 @app.route('/')
-def index():
-    """
-    retourne le contenu de la page index.html
-    """
+def acceuil():
+    """Retourne le contenu de la page index.html."""
     return render_template('index.html')
 
 @app.route('/selection_graphique')
 def selection_graphique():
-    """
-    retourne le contenu de la page graphique.html
-    """
+    """Retourne le contenu de la page graphique.html."""
     return render_template('selection_graphique.html')
+
+@app.route('/auteurs')
+def auteurs():
+    """retourne le contenu de la page index.html"""
+    return render_template('auteurs.html')
+
+
+#-----Les pages de graphiques Chartjs------
 
 @app.route('/graphique_lsinf1252')
 def graphique_lsinf1252():
-    """
-    retourne le contenu de la page graphique.html
-    """
+    """Retourne une page affichant un graphique Chartjs du cours LSINF1252."""
     x_et_y = get_data_from_dict('LSINF1252')
     return page_creator.html_page_creator("LSINF1252 : Nombre d'étudiants par nombre d'essais moyens", 'Page HTML affichant un graphique basé sur les données de la database Inginious', x_et_y[0], x_et_y[1])
 
 @app.route('/graphique_lsinf1101-python')
 def graphique_lsinf1101_python():
-    """
-    retourne le contenu de la page graphique.html
-    """
+    """Retourne une page affichant un graphique Chartjs du cours LSINF1101-PYTHON."""
     x_et_y = get_data_from_dict('LSINF1101-PYTHON')
     return page_creator.html_page_creator("LSINF1101-PYTHON : Nombre d'étudiants par nombre d'essais moyens", 'Page HTML affichant un graphique basé sur les données de la database Inginious', x_et_y[0], x_et_y[1])
 
 @app.route('/graphique_lepl1402')
 def graphique_lepl1402():
-    """
-    retourne le contenu de la page graphique.html
-    """
+    """Retourne une page affichant un graphique Chartjs du cours LEPL1402."""
     x_et_y = get_data_from_dict('LEPL1402')
     return page_creator.html_page_creator("LEPL1402 : Nombre d'étudiants par nombre d'essais moyens", 'Page HTML affichant un graphique basé sur les données de la database Inginious', x_et_y[0], x_et_y[1])
 
 
-@app.route('/auteurs')
-def auteurs():
-    """
-    retourne le contenu de la page index.html
-    """
-    return render_template('auteurs.html')
+
 
 if __name__ == '__main__':
     app.run()
